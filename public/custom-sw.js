75671830da43a9727f80c8b4ac76089201461720
Rob Bamberg <robbamberg@gmail.com>
2017-12-08 15:08:38 +0100

    Communicate to server and log results

--- public/custom-sw.js
+++ public/custom-sw.js
@@ -1,5 +1,10 @@
 self.addEventListener('sync', function(event) {
   const buyEvent = JSON.parse(event.tag);
+  fetch('/buy', {
+      method: "POST",
+      body: JSON.stringify(buyEvent),
+      headers: {"Content-Type":"application/json"}
+  });
   self.registration.showNotification(
     `Bought ${buyEvent.name} for ${buyEvent.expectedPrice}`,
   );
